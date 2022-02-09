f6fcc1a8081b269d87b8e7d7ef4884d56441d7f0
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2022-02-08 17:43:38 -0800

    Displayed the guess history using displayHistory() function

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function playGame(){
   let numberGuess =  document.getElementById('number-guess').value
   displayResult(numberGuess);
   saveGuessHistory(numberGuess);
+  displayHistory();
 }
 
 /**
@@ -101,9 +102,15 @@ function saveGuessHistory(guess) {
  * HINT: use while loop and string concatentation to create a list of guesses
  */
 function displayHistory() {
-  let index; // TODO
+  let index = guesses.length - 1; // TODO
   let list = "<ul class='list-group'>";
   // *CODE GOES BELOW HERE *
+  while(index >= 0){
+    list += "<li class='list-group-item'>" + 
+    "You guessed" + guesses[index] + "</li>";
+    index-=1;
+  }
+
   list += '</ul>'
   document.getElementById("history").innerHTML = list;
 }
